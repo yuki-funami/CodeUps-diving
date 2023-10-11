16823f36cc6ec17979e1f6979f7746001241bad9
yuki-funami <shipwebd@gmail.com>
2023-10-11 23:01:26 +0900

    Third-Commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,13 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   // hamburger
-  // $('.js-hamburger').on('click', function () {
-  //   $(this).toggleClass('is-open');
-  //   $('.js-sp-nav').toggleClass('is-open');
-  //   $('.js-header__logo').toggleClass('is-open');
-  //   $('html').toggleClass('is-fixed');
-  // });
-
-  // 2nd
   $('.js-hamburger').on('click', function () {
-    if($('.js-hamburger').hasClass('is-open')) {
-      $('.js-hamburger').removeClass('is-open');
-      $('.js-sp-nav').fadeOut(250);
-    } else {
-      $('.js-hamburger').addClass('is-open');
-      $('.js-sp-nav').fadeIn(250);
-    }
+    $('.js-header').toggleClass('is-open');
+    // $('html').toggleClass('is-fixed');
   });
+  $('.js-background').on('click', function() {
+    $('.js-header').removeClass('is-open');
+  });
+
 
 });
